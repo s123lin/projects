bad04cbb05af0c758c9aa4d2fcb284c3d3e8119d
s123lin <susie.lin.98@gmail.com>
2019-03-18 12:19:26 -0500

    Update travis_county_search.py

--- TravisCountyRealEstate/travis_county_search.py
+++ TravisCountyRealEstate/travis_county_search.py
@@ -2,8 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 
+#################################
+# Zip Code within Travis County #
+#################################
 search_text = "78759"
 
+########################################################
+# Access Travis County Website and Search for Zip Code #
+########################################################
 driver = webdriver.Chrome()
 driver.get("http://propaccess.traviscad.org/clientdb/?cid=1")
 elem = driver.find_element_by_name("propertySearchOptions:searchText")
@@ -11,14 +17,13 @@ elem.clear()
 elem.send_keys(search_text)
 elem.send_keys(Keys.RETURN)
 
-#prop_ID = driver.find_element_by_xpath("//span[@prop_id]").get_attribute("prop_id")
-#print("Found: " + prop_ID)
-
+#############################################
+# Iterate through all pages in the site and #
+# extract property ID's from page source.   #
+#############################################
 current_URL = driver.current_url
 append_URL = "?rtype=address&page="
-
 soup = BeautifulSoup(driver.page_source, 'lxml')
-
 base_url = "SearchResults.aspx?rtype=address&page="
 length_base = len(base_url)
 total_pages = 1;
@@ -30,14 +35,11 @@ for link in soup.find_all('a'):
 		if int(number) > total_pages:
 			total_pages = int(number)
 			
-#prop_ID_list = []
-
 f = open("property_id.txt","w+")
 
 for data in soup.find_all('span'):
 	extract = data.get('prop_id')
 	if extract != None:
-		#prop_ID_list.append(extract)
 		f.write(str(extract) + "\n")
 		
 for i in range(2,total_pages + 1):
@@ -47,12 +49,6 @@ for i in range(2,total_pages + 1):
 	for data in soupy.find_all('span'):
 		extract = data.get('prop_id')
 		if extract != None:
-			#prop_ID_list.append(extract)
 			f.write(str(extract) + "\n")
 		
-#for item in prop_ID_list:
-#	f.write(str(item) + "\n")	
 f.close
-
-#assert "No results found." not in driver.page_source
-#driver.close()
